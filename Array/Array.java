99e7304cc30994819fca2c4f4e62d6742f95cf52
hbombur <andrew-991@yandex.ru>
2024-02-09 23:15:47 +0300

    Добавлен класс Array в котором реализована задача №3 JAVA Basic

---
+++ Array/Array.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int tmp;
+        int[] numArray = new int[5];
+        for (int i = 0; i < numArray.length; i++) {
+            numArray[i] = (int) (Math.random() * 6);
+        }
+        tmp = numArray[0];
+        numArray[0] = numArray[numArray.length - 1];
+        numArray[numArray.length - 1] = tmp;
+        System.out.print(numArray[0] + numArray[numArray.length / 2]);
+    }
+}
