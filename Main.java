84049c10cc7ba0702fad35bdbb3325315445caf3
hbombur <andrew-991@yandex.ru>
2024-02-09 21:58:10 +0300

    Реализация программы, выводящей сообщение "Hello World!\n" при помощи форматируемого вывода

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Так как не указано точно каким образом вывести сообщение в консоль,
+        мной использован метод printf(). Так же, если необходимо вывести сообщение с переносом каретки
+        в конце строки, можно использовать println(), без переноса каретки - print().
+        */
+        String message = "Hello World!\n";
+        System.out.printf("%s", message);
+    }
+}
